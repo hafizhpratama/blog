c9eb134c11c671b094a4961294dd853b5db4da57
Hafizh Pratama <hafizhpratama99.com>
2020-04-13 20:28:13 +0700

    edit tipis

--- scripts/scrpits.js
+++ scripts/scrpits.js
@@ -4,9 +4,12 @@ function myFunction() {
     var element3 = document.getElementById("footer");
     var element4 = document.getElementById("font-1");
     var element5 = document.getElementById("font-2");
+    var element6 = document.getElementById("font-3");
+
     element.classList.toggle("dark-mode");
     element2.classList.toggle("card");
     element3.classList.toggle("dark-mode");
     element4.classList.toggle("font-black");
     element5.classList.toggle("font-black");
+    element6.classList.toggle("font-black");
 }
